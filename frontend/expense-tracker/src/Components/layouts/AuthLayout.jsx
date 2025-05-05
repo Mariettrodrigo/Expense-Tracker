c0ce43840c8f803b87ddd42cfb93cc90ccb68728
maria <mariyetavishanthi11@gmail.com>
2025-05-05 21:55:24 +0530

    Updated login page

--- frontend/expense-tracker/src/Components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/Components/layouts/AuthLayout.jsx
@@ -47,8 +47,7 @@ const StatsInfoCard = ({ icon, label, value, color }) => {
     <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10">
       
       <div
-        className='w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl' // corrected backticks
-      >
+        className= {`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>  
         {icon}
       </div>
 
--- frontend/expense-tracker/src/Pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/Pages/Auth/Login.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
-import AuthLayout from '../../Components/layouts/AuthLayout'
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import AuthLayout from '../../Components/layouts/AuthLayout';
+import Input from '../../Components/Inputs/Input'; 
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [password,setPasword] = useState("");
-  const [error,setError] = useState(null);
+  const [password, setPassword] = useState(""); 
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
-// handle login form submit
-const handleLogin = async(e) => {}
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
 
   return (
     <AuthLayout>
@@ -18,21 +21,33 @@ const handleLogin = async(e) => {}
         <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>
           Please enter your details to log in
-          </p>
+        </p>
 
-          <form onSubmit={handleLogin}>
-            <input
+        <form onSubmit={handleLogin}>
+          <Input
             value={email}
-            onChange={({target}) => setEmail(target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             label="Email Address"
             placeholder='maria@example.com'
             type='text'
-            />
-
-            </form>
-    </div>
+          />
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            placeholder='Min 8 Characters'
+            type='password'
+          />
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-primary text-white rounded"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </AuthLayout>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
