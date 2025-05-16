9cf1f8fb5eca38723283fc601a79be5a7745e4bb
maria <mariyetavishanthi11@gmail.com>
2025-05-16 12:36:30 +0530

    updated

--- frontend/expense-tracker/src/Pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/Pages/Auth/SignUp.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../Components/layouts/AuthLayout';
+import Input from '../../Components/Inputs/Input'; 
+import { Link, useNavigate } from 'react-router-dom';
+import { validateEmail } from '../../Utils/helper';
+
 
 const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //handle signup form submit
+
   return (
     <div>
       SignUp
